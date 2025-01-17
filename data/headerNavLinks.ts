a505fc319321ddffe56716dbf3bcf4216a8a3363
Rawbinn Shrestha <rawbinnn@gmail.com>
2025-01-17 22:36:43 +0545

    Added projects

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/about', title: 'About Me' },
+  { href: '/about', title: 'About' },
+  { href: '/projects', title: 'Projects' },
   { href: '/blogs', title: 'Blog' },
 ]
 
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,27 +1,31 @@
-interface Project {
+export interface Project {
   title: string
   description: string
   href?: string
   imgSrc?: string
+  techStacks?: string[]
+  github?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'RAWBINN.COM Personal Blog',
+    description: `RAWBINN.COM is a personal blog that focuses on sharing my knowledge and experiences. It was created using Next.js and Tailwind.`,
+    imgSrc: '/static/images/projects/rawbinn_dot_com.png',
+    href: 'https://rawbinn.com',
+    github: 'https://github.com/rawbinn/rawbinn.com',
+    techStacks: ['Next.js', 'Tailwind', 'Typescript', 'MDX'],
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    title: 'CoinSense: Money Tracking App',
+    description: `CoinSense is built using Next.js and Laravel. It is a simple and efficient
+    money tracking app that allows you to monitor your day-to-day income, expenses,
+    loans, and group expenditures. Additionally, it features a reminder system to help you
+    stay on top of your finances.`,
+    imgSrc: '/static/images/projects/coinsense.png',
+    href: 'https://coinsense.rawbinn.com',
+    techStacks: ['NextJs', 'Laravel', 'Bootstrap'],
+  }
 ]
 
 export default projectsData
